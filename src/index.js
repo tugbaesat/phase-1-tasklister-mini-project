6cb3707668396218cb6ca4f4840cbf98ed4c8b72
Tugba Esat <tugbaesat007@gmail.com>
2023-04-18 16:47:42 +0300

    required deliverables done, delete function added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    implementToDos();
+    form.reset();
+  });
+
+  function implementToDos() {
+    const input = document.getElementById("new-task-description");
+    const description = input.value;
+    const liElement = document.createElement("li");
+    const ulElement = document.getElementById("tasks");
+    const button = document.createElement("button");
+
+    liElement.textContent = `${description} `;
+    ulElement.append(liElement);
+
+    button.textContent = "x";
+    liElement.appendChild(button);
+
+    button.addEventListener("click", deleteToDos);
+  }
+
+  function deleteToDos(e) {
+    e.target.parentNode.remove();
+  }
 });
